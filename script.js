27aa5f1e9cc118f405f331f223bdb4a70aeb7906
Tien Trinh <tqtien.a116169@gmail.com>
2022-01-30 00:33:57 +0900

    Uncomment js file

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// const data = fetch('./data.json')
-// 	.then(res => res.json())
-// 	.then(data => console.log(data))
-// 	.catch(err => console.error('Error: ' + err))
+const data = fetch('./data.json')
+	.then(res => res.json())
+	.then(data => console.log(data))
+	.catch(err => console.error('Error: ' + err))
