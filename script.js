058ea1c307269b0ec32fb1e8812fc43eebac9f70
Tien Trinh <tqtien.a11619@gmail.com>
2022-06-20 23:16:12 +0700

    Add class for handling data with js

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+// const test = document.querySelector("#test");
 // const data = fetch('./data.json')
 // 	.then(res => res.json())
-// 	.then(data => console.log(data))
+// 	.then(data => {
+// 		console.log(data[0].timeframes.daily.current)
+// 		test.innerHTML = data[0].timeframes.daily.current + "hrs";
+
+// 	})
 // 	.catch(err => console.error('Error: ' + err))
 
+async function printJSON() {
+	const response = await fetch("data.json");
+	const json = await response.json();
+	console.log(json);
+}
+
+printJSON();
