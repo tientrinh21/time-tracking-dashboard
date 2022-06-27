c5a4625424583ffd8fafc09aee0fa22363526360
Tien Trinh <tqtien.a11619@gmail.com>
2022-06-27 14:05:06 +0900

    query all data field

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-// const test = document.querySelector("#test");
-// const data = fetch('./data.json')
-// 	.then(res => res.json())
-// 	.then(data => {
-// 		console.log(data[0].timeframes.daily.current)
-// 		test.innerHTML = data[0].timeframes.daily.current + "hrs";
+// index 0 is current-data and index 1 is previous-data
+const work = document.querySelectorAll(".work")
+const play = document.querySelectorAll(".play")
+const study = document.querySelectorAll(".study")
+const exercise = document.querySelectorAll(".exercise")
+const social = document.querySelectorAll(".social")
+const selfCare = document.querySelectorAll(".self-care")
 
-// 	})
-// 	.catch(err => console.error('Error: ' + err))
+const dailyBtn = document.querySelector("#daily-btn");
+const monthlyBtn = document.querySelector("#monthly-btn");
+const yearlyBtn = document.querySelector("#yearly-btn");
 
-async function printJSON() {
+async function getData() {
 	const response = await fetch("data.json");
-	const json = await response.json();
-	console.log(json);
+	const json = await response.json()
+	return json;
 }
 
-printJSON();
+getData().then(console.log)
+
+
+
